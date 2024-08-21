f227e691aeff99fa8530ba8398ad5b2200580815
PEDRO HENRIQUE NUNES ZANETTE <pedro_zanette@estudante.sesisenai.org.br>
2024-08-21 09:56:44 -0300

    Arrays

--- js/script.js
+++ js/script.js
@@ -60,3 +60,151 @@ console.log(person.name.length);
 
 console.log(typeof person);
 
+// 6 - criando e deletando propriedades
+const car = {
+    engine: 2.0,
+    brand: "VW",
+    model: "Tiguan",
+    km: 20000,
+}
+
+console.log(car);
+
+car.doors = 4;
+
+console.log(car);
+
+delete car.km;
+
+console.log(car);
+
+
+// 7 - mais sobre objetos
+const obj = {
+    a: "teste",
+    b: true,
+}
+
+console.log(obj instanceof Object);
+
+
+const obj2 = {
+    c: [],
+};
+
+Object.assign(obj2, obj);
+
+console.log(obj2);
+
+console.log(obj);
+
+// 8 - conhecendo melhor os objetos
+console.log(Object.keys(obj));
+console.log(Object.keys(obj2));
+console.log(Object.keys(car));
+
+console.log(Object.entries(car));
+
+// 9 - mutação
+const a = {
+    name: "Pedro",
+};
+
+const b = a;
+
+console.log(a);
+console.log(b);
+
+console.log(a === b);
+
+a.age = 17;
+
+console.log(b);
+
+delete b.age;
+
+console.log(a);
+console.log(b);
+
+// 10 - loop em array
+const users = ["Pedro","João","Vitor","Gabriel"]
+
+for(let i = 0; i < users.length; i++){
+    console.log(`Listabdo o usuário: ${users[i]}`);
+}
+
+// 11 - push e pop
+const array = ["a","b","c"];
+
+array.push("d");
+
+console.log(array);
+
+console.log(array.length);
+
+array.pop();
+
+console.log(array);
+
+const itemRemovido = array.pop();
+
+console.log(itemRemovido);
+
+console.log(array);
+
+array.push("z","x","y");
+
+console.log(array);
+
+
+// 12 - shift e unshift
+
+const letters = ["a","b","c"];
+
+const letter = letters.shift();
+
+console.log(letter);
+
+console.log(letters);
+
+letters.unshift("p","q","r");
+
+letters.unshift("z");
+
+console.log(letters);
+
+
+// 13 - indexOf e lastIndexOf
+const myElements = ["Morango", "Maçã", "Abacate","Pera","Abacate"];
+
+console.log(myElements.indexOf("Maçã"));
+console.log(myElements.indexOf("Abacate"));
+
+console.log(myElements[2]);
+
+console.log(myElements[myElements.indexOf("Abacate")]);
+
+console.log(myElements.lastIndexOf("Abacate"));
+
+console.log(myElements.indexOf("Mamão"));
+
+// 14 - slice
+const testeSlice = ["a","b","c","d","f"];
+
+const subArray = testeSlice.slice(2, 4);
+
+console.log(subArray);
+
+console.log(testeSlice);
+
+const subArray2 = testeSlice.slice(2, 4 + 1);
+
+console.log(subArray2);
+
+const subArray3 = testeSlice.slice(10,20);
+
+console.log(subArray3);
+
+const subArray4 = testeSlice.slice(2);
+
+console.log(subArray4);
